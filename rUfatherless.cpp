d2364a0ca9bf19347da8493f56f3631edc5d409f
budgiemint <70620754+snowythebudg@users.noreply.github.com>
2023-01-20 20:52:02 -0700

    cpp

---
+++ rUfatherless.cpp
@@ -0,0 +1,8 @@
+// uRfatherless - this is the new test version since the last one doesnt work
+#include <iostream>
+
+int main() {
+    std::cout << "you are fatherless and this is something you would never use and also a garbage windows app that no one would ever want but who cares | ";
+    system("pause"); 
+    return 0;
+}
